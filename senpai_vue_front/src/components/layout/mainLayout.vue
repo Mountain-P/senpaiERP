b598d0646eeeeb42244cd6a28c485ea782d12ff5
Mountain-P <lspssapple@gmail.com>
2022-05-14 00:51:39 +0800

    202205121251

--- senpai_vue_front/src/components/layout/mainLayout.vue
+++ senpai_vue_front/src/components/layout/mainLayout.vue
@@ -4,7 +4,7 @@
     <Sidebar />
     <v-main class="content">
       <router-view />
-      <Footer />
+      <!-- <Footer /> -->
     </v-main>
   </v-app>
 </template>
@@ -12,10 +12,10 @@
 <script>
 import Header from "@/components/Header/Header";
 import Sidebar from "@/components/Sidebar/Sidebar";
-import Footer from "@/components/Footer/Footer";
+// import Footer from "@/components/Footer/Footer";
 export default {
   name: "mainLayout",
-  components: { Footer, Header, Sidebar },
+  components: { Header, Sidebar },
 };
 </script>
 
--- senpai_vue_front/src/views/dashboardView.vue
+++ senpai_vue_front/src/views/dashboardView.vue
@@ -1,20 +1,6 @@
 <template>
   <v-container fluid>
     <div class="dashboard-page">
-      <v-row no-gutters class="d-flex justify-space-between mt-10 mb-6">
-        <h1 class="page-title">Dashboard</h1>
-        <v-menu offset-y>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              v-bind="attrs"
-              v-on="on"
-              color="secondary"
-              class="text-capitalize button-shadow mr-1"
-              >Latest Reports</v-btn
-            >
-          </template>
-        </v-menu>
-      </v-row>
       <v-row>
         <v-col lg="3" sm="6" md="5" cols="12">
           <v-card class="mx-1 mb-1">
