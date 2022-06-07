3ec12d7ac711cccc067c81ef6bea1ead6b0deb2a
MountainP <lspssapple@gmail.com>
2022-06-07 20:15:00 +0800

    nb

--- senpai_vue_front/src/views/employee/salaryManage.vue
+++ senpai_vue_front/src/views/employee/salaryManage.vue
@@ -1,8 +1,20 @@
 <template>
     <v-container>
         <v-row>
-            <v-col col="12" md="12" sm="12">
-                <v-card max-width="300px"></v-card>
+            <v-col cols="12" md="3" sm="12">
+                <v-card>
+                    <v-card-title>員工選擇</v-card-title>
+                </v-card>
+            </v-col>
+            <v-col cols="12" md="6" sm="12">
+                <v-card>
+                    <v-card-title>出班紀錄</v-card-title>
+                </v-card>
+            </v-col>
+            <v-col cols="12" md="3" sm="12">
+                <v-card>
+                    <v-card-title>本月薪資總結</v-card-title>
+                </v-card>
             </v-col>
         </v-row>
     </v-container>
--- senpai_vue_front/src/views/employee/tables.vue
+++ senpai_vue_front/src/views/employee/tables.vue
@@ -1,143 +1,97 @@
 <template>
   <v-container>
-    <v-data-table :headers="headers" :items="contents" class="elevation-1">
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title>員工列表</v-toolbar-title>
-          <v-divider class="mx-4" inset vertical></v-divider>
-          <v-spacer></v-spacer>
+    
+        <v-data-table :headers="headers" :items="contents" class="elevation-1">
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>員工列表</v-toolbar-title>
+              <v-divider class="mx-4" inset vertical></v-divider>
+              <v-spacer></v-spacer>
 
-          <!-- 新增/編輯 對話視窗 -->
-          <v-dialog v-model="dialog" max-width="500px">
-            <template v-slot:activator="openDialogButton">
-              <v-btn
-                color="primary"
-                dark
-                class="mb-2"
-                v-bind="openDialogButton.attrs"
-                v-on="openDialogButton.on"
-              >
-                新增員工
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">{{ formTitle }}</span>
-              </v-card-title>
+              <!-- 新增/編輯 對話視窗 -->
+              <v-dialog v-model="dialog" max-width="500px">
+                <template v-slot:activator="openDialogButton">
+                  <v-btn color="primary" dark class="mb-2" v-bind="openDialogButton.attrs" v-on="openDialogButton.on">
+                    新增員工
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">{{ formTitle }}</span>
+                  </v-card-title>
 
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="12" md="12">
-                      <v-text-field
-                        v-model="editedItem.number"
-                        label="員工編號"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="12" md="12">
-                      <v-text-field
-                        v-model="editedItem.postion"
-                        label="職稱"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="6" sm="12" md="6">
-                      <v-text-field
-                        v-model="editedItem.name"
-                        label="姓名"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="6" sm="12" md="6">
-                      <v-text-field
-                        v-model="editedItem.phone"
-                        label="連絡電話"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="6" sm="12" md="6">
-                      <v-text-field
-                        v-model="editedItem.idNumber"
-                        label="身分證字號"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="6" sm="12" md="6">
-                      <v-menu
-                        v-model="datePicker"
-                        :close-on-content-click="false"
-                        :nudge-right="40"
-                        transition="scale-transition"
-                        offset-y
-                        min-width="auto"
-                      >
-                        <template v-slot:activator="datePickerBind">
-                          <v-text-field
-                            v-model="editedItem.birthday"
-                            label="生日"
-                            readonly
-                            v-bind="datePickerBind.attrs"
-                            v-on="datePickerBind.on"
-                          ></v-text-field>
-                        </template>
-                        <v-date-picker
-                          v-model="editedItem.birthday"
-                          @input="datePicker = false"
-                        ></v-date-picker>
-                      </v-menu>
-                    </v-col>
-                    <v-col cols="12" sm="12" md="12">
-                      <v-text-field
-                        v-model="editedItem.baseSalary"
-                        label="底薪"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="6" sm="12" md="6">
-                      <v-file-input
-                        v-model="editedItem.idPictureUpside"
-                        accept="image/*"
-                        label="身分證正面檔案"
-                      ></v-file-input>
-                    </v-col>
-                    <v-col cols="6" sm="12" md="6">
-                      <v-file-input
-                        v-model="editedItem.idPictureDownside"
-                        accept="image/*"
-                        label="身分證反面檔案"
-                      ></v-file-input>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="12" md="12">
+                          <v-text-field v-model="editedItem.number" label="員工編號"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="12" md="12">
+                          <v-text-field v-model="editedItem.postion" label="職稱"></v-text-field>
+                        </v-col>
+                        <v-col cols="6" sm="12" md="6">
+                          <v-text-field v-model="editedItem.name" label="姓名"></v-text-field>
+                        </v-col>
+                        <v-col cols="6" sm="12" md="6">
+                          <v-text-field v-model="editedItem.phone" label="連絡電話"></v-text-field>
+                        </v-col>
+                        <v-col cols="6" sm="12" md="6">
+                          <v-text-field v-model="editedItem.idNumber" label="身分證字號"></v-text-field>
+                        </v-col>
+                        <v-col cols="6" sm="12" md="6">
+                          <v-menu v-model="datePicker" :close-on-content-click="false" :nudge-right="40"
+                            transition="scale-transition" offset-y min-width="auto">
+                            <template v-slot:activator="datePickerBind">
+                              <v-text-field v-model="editedItem.birthday" label="生日" readonly
+                                v-bind="datePickerBind.attrs" v-on="datePickerBind.on"></v-text-field>
+                            </template>
+                            <v-date-picker v-model="editedItem.birthday" @input="datePicker = false"></v-date-picker>
+                          </v-menu>
+                        </v-col>
+                        <v-col cols="12" sm="12" md="12">
+                          <v-text-field v-model="editedItem.baseSalary" label="底薪"></v-text-field>
+                        </v-col>
+                        <v-col cols="6" sm="12" md="6">
+                          <v-file-input v-model="editedItem.idPictureUpside" accept="image/*" label="身分證正面檔案">
+                          </v-file-input>
+                        </v-col>
+                        <v-col cols="6" sm="12" md="6">
+                          <v-file-input v-model="editedItem.idPictureDownside" accept="image/*" label="身分證反面檔案">
+                          </v-file-input>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close"> 取消 </v-btn>
-                <v-btn color="blue darken-1" text @click="save"> 儲存 </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <!-- 新增/編輯 對話視窗 -->
-          <!-- 刪除 對話視窗 -->
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="text-h5">確定要刪除此項目?</v-card-title>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete"
-                  >取消</v-btn
-                >
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                  >確定</v-btn
-                >
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <!-- 刪除 對話視窗 -->
-        </v-toolbar>
-      </template>
-      <template v-slot:[`item.actions`]="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-      </template>
-    </v-data-table>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="close"> 取消 </v-btn>
+                    <v-btn color="blue darken-1" text @click="save"> 儲存 </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <!-- 新增/編輯 對話視窗 -->
+              <!-- 刪除 對話視窗 -->
+              <v-dialog v-model="dialogDelete" max-width="500px">
+                <v-card>
+                  <v-card-title class="text-h5">確定要刪除此項目?</v-card-title>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="closeDelete">取消</v-btn>
+                    <v-btn color="blue darken-1" text @click="deleteItemConfirm">確定</v-btn>
+                    <v-spacer></v-spacer>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <!-- 刪除 對話視窗 -->
+            </v-toolbar>
+          </template>
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+            <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+          </template>
+        </v-data-table>
+
   </v-container>
 </template>
 
@@ -147,7 +101,7 @@ export default {
   name: "EmployeeManageTables",
   data() {
     return {
-      dialog: false,
+    dialog: false,
       dialogDelete: false,
       datePicker: false,
       headers: [
@@ -262,4 +216,5 @@ export default {
   },
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+</style>
